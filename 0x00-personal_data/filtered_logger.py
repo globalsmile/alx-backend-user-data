b207fd3db66d3a3ef1737440c71c33d6ea2e2a23
globalsmile <mohammedbadmus823@gmail.com>
2024-02-08 14:31:24 +0100

    fix pycodestyle for filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,7 +56,7 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.environ.get("PERSONAL_DATA_DB_NAME","")
+    db_name = os.environ.get("PERSONAL_DATA_DB_NAME", "")
     return mysql.connector.connect(
         user=username,
         password=password,
